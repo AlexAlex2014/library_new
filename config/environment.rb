9c88473cc01cebb0981e486370f3c080953bd11d
Alex <dev1zuev@gmail.com>
2019-12-26 17:41:11 +0200

    next

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,10 @@
+ENV.delete("DATABASE_URL")
+
 # Load the Rails application.
 require_relative 'application'
 
 # Initialize the Rails application.
+
 
 app_env_vars = File.join(Rails.root, 'config', 'initializers', 'app_env_vars.rb')
 load(app_env_vars) if File.exists?(app_env_vars)
